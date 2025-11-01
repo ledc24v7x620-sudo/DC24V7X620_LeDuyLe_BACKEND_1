4941a5162fa86e4e1d9553d5c4e4062c461504af
ledc24v7x620-sudo <ledc24v7x620@dttx.ctu.edu.vn>
2025-11-01 21:22:53 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Route kiểm tra hoạt động
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to DC24V7X620 - Le Duy Le Backend!" });
 });
-
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
